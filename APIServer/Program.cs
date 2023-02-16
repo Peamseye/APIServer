54de2c48e46aa098d0ac84c4356f3c516875a1cf
Peamseye <aispotato587@gmail.com>
2023-02-16 10:17:49 +0100

    criminal moment

--- APIServer/Program.cs
+++ APIServer/Program.cs
@@ -5,16 +5,15 @@ app.Urls.Add("http://*:3000");
 
 List<Supervillain> villains = new();
 
-Supervillain penguin = new Supervillain();
-penguin.Name = "Penguin";
-penguin.Appearance = "Short";
-penguin.Strength = 50;
+villains.Add(new() { Name = "Penguin", Appearance = "Short with top hat", Strength = 50});
+villains.Add(new() { Name = "Joker", Appearance = "Tall and with clown outfit", Strength = 150});
+villains.Add(new() { Name = "Two-Face", Appearance = "Split personality & looks", Strength = 100});
 
 app.MapGet("/", Answer);
 
-app.MapGet("/Supervillain/", () => 
+app.MapGet("/supervillain/", () => 
 {
-    return penguin;
+    return villains;
 });
 
 app.Run();
